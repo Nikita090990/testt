8a91e9463770607f70576bd7661305d5e85b01ca
Rostislav2007 <kukolevrostislav@gmail.com>
2024-07-01 19:00:29 +0300

    chenge fanction print

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print();
+void sum(int a, int b);
+
+int main()
+{
+    
+    return 0;
+}
+
+void print()
+{
+    cout << "Hello C++" << endl;
+}
+
+void sum(int a, int b)
+{
+    cout << a+b << endl;
+}
+
+
+
+/* g++ main.cpp
+./a.exe */
